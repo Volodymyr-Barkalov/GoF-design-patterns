3266e70a47f2410e55de6b6cdb61e31530c64cf0
Volodymyr Barkalov <vobarkal@gmail.com>
2020-11-29 18:12:05 +0200

    added missed implementation for composite

--- src/main/java/com/barkalov/structural/composite/ShapeCreator.java
+++ src/main/java/com/barkalov/structural/composite/ShapeCreator.java
@@ -2,6 +2,25 @@ package com.barkalov.structural.composite;
 
 public class ShapeCreator {
     public static void main(String[] args) {
-//        Shape round
+        Composite composite1 = new Composite();
+        composite1.addShape(new Square());
+        composite1.addShape(new Round());
+        composite1.addShape(new Square());
+        composite1.addShape(new Triangle());
+
+        Composite composite2 = new Composite();
+        composite2.addShape(new Square());
+        composite2.addShape(new Round());
+
+        Composite composite3 = new Composite();
+        composite3.addShape(new Triangle());
+        composite3.addShape(new Triangle());
+
+        Composite mainComposite = new Composite();
+        mainComposite.addShape(composite1);
+        mainComposite.addShape(composite2);
+        mainComposite.addShape(composite3);
+
+        mainComposite.draw();
     }
 }
